b11c9cbe253719a9e34143060665b2a9e03954f9
zewo2 <191111034+zewo2@users.noreply.github.com>
2025-05-27 21:03:52 +0100

    controller resources

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MovieController extends Controller
+class DashboardController extends Controller
 {
     /**
      * Display a listing of the resource.
